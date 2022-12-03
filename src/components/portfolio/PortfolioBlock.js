1dfd579404f4993a5164ce38509e0fc9514fd5a1
TrakaDev <trakadev@gmail.com>
2022-12-03 00:46:28 -0600

    [ADD] Box Style for Projects

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -5,8 +5,8 @@ import {Box} from "@mui/material";
 function PortfolioBlock(props) {
    const {image, live, source, title} = props;
    return (
-      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
-         <Box component={'img'} src={image} alt={'mockup'}/>
+      <Box sx={{paddingTop: "10vh"}} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
+         <Box sx={{maxWidth: "35vw"}} component={'img'} src={image} alt={'mockup'}/>
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
